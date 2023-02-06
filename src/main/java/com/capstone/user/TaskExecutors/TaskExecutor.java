567424a46511381beaa5221e678d290572e97bc8
AJ Pongress <ajpongress@gmail.com>
2023-02-06 15:06:45 -0800

    Deleted unused commented code

--- src/main/java/com/capstone/user/TaskExecutors/TaskExecutor.java
+++ src/main/java/com/capstone/user/TaskExecutors/TaskExecutor.java
@@ -2,7 +2,6 @@ package com.capstone.user.TaskExecutors;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.task.SimpleAsyncTaskExecutor;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Component;
 
 @Component
